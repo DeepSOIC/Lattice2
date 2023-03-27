659345b1e9d3f6613984d7ca269306e78a1ecab0
DeepSOIC <vv.titov@gmail.com>
2023-03-28 01:37:45 +0200

    icons: fix errors in FC0.17

--- PyResources/recompile_rc.bat
+++ PyResources/recompile_rc.bat
@@ -1,4 +1,4 @@
 pyside-rcc icons.qrc -py3 -o ..\lattice2_rc.py
-rcc wb-icon.qrc --binary --o wb-icon.rcc
+pyside-rcc wb-icon.qrc -py3 -o ..\lattice2_rc_minimal.py
 echo Should be compiled now =)
 pause
