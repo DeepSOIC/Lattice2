58a1cfdd008c66625ee63185c4e544408f643553
Noddycode <neorescuer@gmail.com>
2025-11-19 16:16:27 -0500

    Add tests for simple lattice placement. Squash commits

---
+++ TestLattice2Gui.py
@@ -0,0 +1,15 @@
+import FreeCAD
+
+'''
+Tests imported in this file are intended to be run via the Test Framework WB.
+
+To run test:
+1. Load Test Framework WB
+2. Click "Self-test"
+3. Find "TestLattice2Gui" in test name dropdown
+4. Click "Start"
+
+Alternatively, run "FreeCAD -t TestLattice2Gui" or "/path/to/FreeCAD/binary -t TestLattice2Gui" from your OS terminal
+'''
+
+from test.gui.TestLatticePlacement import TestLatticePlacement
---
+++ test/gui/TestLatticePlacement.py
@@ -0,0 +1,140 @@
+import unittest
+
+import FreeCAD as App
+import FreeCADGui as Gui
+
+import lattice2ArrayFromShape
+import lattice2Executer
+import lattice2Placement
+
+
+class TestLatticePlacement(unittest.TestCase):
+    """ Unit tests for single lattice placement objects in FreeCAD. """
+
+    def setUp(self):
+        self.doc = App.newDocument("TestLatticePlacement")
+
+    def tearDown(self):
+        self.doc.clearDocument()
+
+    def test_basic_single_lattice_placement(self):
+        """ Test creation and rotation of basic single lattice placement objects. """
+        # Uses Yaw-Pitch-Roll (ZYX) rotation convention
+        placementChoices = {
+            "Custom": App.Rotation(0, 0, 0),
+            "XY plane": App.Rotation(0, 0, 0),
+            "XZ plane": App.Rotation(0, 0, 90),
+            "YZ plane": App.Rotation(90, 0, 90),
+        }
+
+        for placementChoice, rotation in placementChoices.items():
+            with self.subTest(placement_type=placementChoice):
+                placementName = f"{placementChoice.replace(' ', '_')}_Placement"
+                placement = lattice2Placement.makeLatticePlacement(name=placementName)
+                placement.PlacementChoice = placementChoice
+                self.doc.recompute()
+
+                placement = self.doc.getObject(placementName)
+
+                self.assertIsNotNone(placement)
+                self.assertEqual(placementName, placement.Name)
+                self.assertEqual(placementChoice, placement.PlacementChoice)
+                self.assertTrue(
+                    placement.Placement.Rotation.isSame(rotation, 0.1))  # Allow small tolerance for rounding errors
+                self.assertEqual(1, placement.NumElements)
+
+    def test_single_lattice_placement_along_axis(self):
+        """ Test basic creation and rotation of single lattice placement objects along specific axes.
+
+            Mimics default values for "along X", "along Y", and "along Z" single placements from GUI buttons.
+        """
+
+        # Key: The single placement name
+        # Value: Tuple of XDir_wanted and expected position in Yaw-Pitch-Roll (ZYX) rotation convention
+        alongAxisOptions = {
+            "along X": (App.Vector(1, 0, 0), App.Rotation(0, 0, 0)),
+            "along Y": (App.Vector(0, 1, 0), App.Rotation(90, 0, 0)),
+            "along Z": (App.Vector(0, 0, 1), App.Rotation(0, -90, 180)),
+        }
+
+        defaultPriority = "XZY"
+
+        for alongAxis, rotationTuple in alongAxisOptions.items():
+            with self.subTest(along_axis=alongAxis):
+                placementName = f"Lattice_Placement_{alongAxis.replace(' ', '_')}"
+                placement = lattice2Placement.makeLatticePlacementAx(placementName)
+                placement.XDir_wanted = rotationTuple[0]
+                placement.Priority = defaultPriority
+                self.doc.recompute()
+
+                placement = self.doc.getObject(placementName)
+
+                self.assertIsNotNone(placement)
+                self.assertEqual(placementName, placement.Name)
+                self.assertEqual(rotationTuple[0], placement.XDir_actual)
+                self.assertTrue(placement.Placement.Rotation.isSame(rotationTuple[1], 0.1))
+                self.assertEqual(1, placement.NumElements)
+
+    def test_single_lattice_placement_euler_angles(self):
+        """ Test basic creation and rotation of single lattice placement objects using Euler angles."""
+
+        # Test rotations in Yaw-Pitch-Roll (ZYX) convention
+        testRotations = [
+            (0, 0, 0),
+            (45, 30, 60),
+            (90, 45, 30),
+            (180, 90, 45),
+            (-45, -30, -60),
+        ]
+
+        for testRotations in testRotations:
+            with self.subTest(test_rotation=testRotations):
+                angleNames = [str(angle).replace('-', 'neg') for angle in testRotations]
+                placementName = f"Lattice_Placement_Euler_{'_'.join(angleNames)}"
+                placement = lattice2Placement.makeLatticePlacementEuler(placementName)
+                placement.Yaw = testRotations[0]
+                placement.Pitch = testRotations[1]
+                placement.Roll = testRotations[2]
+                self.doc.recompute()
+
+                placement = self.doc.getObject(placementName)
+
+                self.assertIsNotNone(placement)
+                self.assertEqual(placementName, placement.Name)
+                self.assertTrue(placement.Placement.Rotation.isSame(App.Rotation(*testRotations), 0.1))
+                self.assertEqual(1, placement.NumElements)
+
+    def test_lattice_placement_from_shape(self):
+        """ Test creation and of single lattice placement objects from shape geometry.
+
+            Mimics default values for the following "Placement of shape" GUI buttons:
+            "copy object Placement", "center of bounding box", "center of mass", and "inertial axis system".
+        """
+        cube = self.doc.addObject("Part::Box", "BaseCube")
+        cube.Placement.Base = App.Vector(50, 100, 150)
+        self.doc.recompute()
+
+        positionChoices = {
+            "child": cube.Placement.Base,  # Copy object Placement
+            "child.CenterOfMass": cube.Shape.CenterOfMass,  # Center of mass
+            "child.CenterOfBoundBox": cube.Shape.BoundBox.Center,  # Center of bounding box
+            "child.Vertex": cube.Shape.Vertexes[0].Point,  # Inertial axis system
+        }
+        print(positionChoices)
+
+        for translateMode, expectedPosition in positionChoices.items():
+            with self.subTest(translate_mode=translateMode):
+                placementName = f"Lattice_Placement_From_Shape_{translateMode.replace('.', '_')}"
+                placement = lattice2ArrayFromShape.makeLatticeArrayFromShape(placementName)
+                placement.ShapeLink = cube
+                placement.TranslateMode = translateMode
+                placement.TranslateElementIndex = 1  # Index is decremented by 1 to reference vertex
+                placement.ExposePlacement = True
+                self.doc.recompute()
+
+                placement = self.doc.getObject(placementName)
+
+                self.assertIsNotNone(placement)
+                self.assertEqual(placementName, placement.Name)
+                self.assertEqual(expectedPosition, placement.Placement.Base)
+                self.assertEqual(1, placement.NumElements)
