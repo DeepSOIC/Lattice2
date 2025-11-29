9af346102e5872f14a72fe1851deb3f0b3ee9135
Bozo Dragojevic <bozzo@digiverse.si>
2025-11-29 17:56:37 +0100

    Fix logic error in strrev parsing
    `'(Git)' or '(Git shallow)' in strrev` is always true because `'(Git)'` is a non-empty string.

--- lattice2Compatibility.py
+++ lattice2Compatibility.py
@@ -15,7 +15,7 @@ def get_fc_version():
         rev = 0
     if '(GitTag)' in strrev:
         submi = int(strrev.split(" ")[0])
-    elif '(Git)' or '(Git shallow)' in strrev:
+    elif '(Git)' in strrev or '(Git shallow)' in strrev:
         try:
             rev = int(strrev.split(" ")[0])
         except Exception as err:
